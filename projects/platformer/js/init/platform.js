fa388d3a6ee2144fd90b227f7b127c7f86ea0d2f
Duck8608 <ilikenintendo97@gmail.com>
2021-06-08 16:26:11 +0000

    platform

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -37,7 +37,7 @@
         createPlatform(0, game.world.height - 32, 3, 2);    // DO NOT DELETE
         
         
-        createPlatform(-200, 750, 0.2, 0.1, true);
+        createPlatform(200, 750, 0.2, 0.1, true);
         
         
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
