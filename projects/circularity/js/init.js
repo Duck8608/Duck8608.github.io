5cb70b33213237a34ba8b5ff0a32f3a73b163f85
Duck8608 <ilikenintendo97@gmail.com>
2021-06-18 15:50:42 +0000

    todo two

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -24,11 +24,19 @@ var init = function (window) {
         var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        function drawCircle() {}
+        function drawCircle() {
+
+            // Code to draw a circle
+circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+physikz.addRandomVelocity(circle, canvas);
+view.addChild(circle);
+circles.push(circle);
+
+         }
 
         // TODO 3 / 8 : Call the drawCircle() function 
         
-        for (var loopCompleted = 0)
+        for (var loopCompleted = 0; loopCompleted < 100; loopCompeted++)
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
